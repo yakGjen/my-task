4f679958afaaae65e20ab1c8a523189a8cbb1155
yak.gjen <yak.gjen@gmail.com>
2020-06-17 01:47:28 +0300

    fix: replace class on functions with hooks in a form component

--- front/src/Main/Form/Form.js
+++ front/src/Main/Form/Form.js
@@ -1,62 +1,55 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 
 import './Form.scss';
 
-class Form extends Component {
-  state = {
-    inputValue: ''
+const sendData = async (dataStr, e) => {
+  e.preventDefault();
+
+  if (dataStr === '') {
+    alert('Enter word or sentence in a text field');
+    return;
   }
 
-  handleInputValue = (event) => {
-    this.setState({inputValue: event.target.value});
-  }
+  const sendData = JSON.stringify({inputValue: dataStr});
 
-  sendData = async (event) => {
-    event.preventDefault();
-
-    if (this.state.inputValue === '') {
-      alert('Enter word or sentence in a text field');
-      return;
+  const data = await fetch('http://localhost:4200/main', {
+    method: 'POST',
+    body: sendData,
+    headers: {
+      'Content-Type': 'application/json'
     }
+  });
 
-    const sendData = JSON.stringify(this.state);
+  const result = await data.json();
 
-    const data = await fetch('http://localhost:4200/main', {
-      method: 'POST',
-      body: sendData,
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+  console.log(result);
+};
 
-    const result = await data.json();
+const showData = async () => {
+  const data = await fetch('http://localhost:4200/main');
+  const result = await data.json();
 
-    this.setState({inputValue: ''});
-    console.log(result);
-  }
+  console.log('all data:', result);
+};
 
-  showData = async () => {
-    const data = await fetch('http://localhost:4200/main');
-    const result = await data.json();
-
-    console.log('all data:', result);
-  };
-
-  render() {
-    return (
-      <form className='form' onSubmit={this.sendData}>
-        <input 
-          type='name' 
-          placeholder='Enter your request'
-          className='form__input'
-          onChange={this.handleInputValue}
-          value={this.state.inputValue}
-        />
-        <button type='button' className='form__button' onClick={this.showData}>show all requests</button>
-        <button type='submit' className='form__button'>send data</button>
-      </form>
-    );
-  }
-}
+const Form = () => {
+  const [inputValue, setInputValue] = useState('');
+  return (
+    <form className='form' onSubmit={(e) => {
+      setInputValue('');
+      return sendData(inputValue, e);
+    }}>
+      <input 
+        type='name' 
+        placeholder='Enter your request'
+        className='form__input'
+        onChange={(e) => setInputValue(e.target.value)}
+        value={inputValue}
+      />
+      <button type='button' className='form__button' onClick={showData}>show all requests</button>
+      <button type='submit' className='form__button'>send data</button>
+    </form>
+  );
+};
 
 export default Form;
